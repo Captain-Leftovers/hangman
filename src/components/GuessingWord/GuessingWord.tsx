cf8cedf1d417afb1e8dc5a6c81ed4fce2623b35b
Dob <lalilulelo.mg@gmail.com>
2022-12-10 18:32:09 +1100

    adjust styles on layout

--- src/components/GuessingWord/GuessingWord.tsx
+++ src/components/GuessingWord/GuessingWord.tsx
@@ -1,14 +1,14 @@
 import GuessingWordCSS from './GuessingWordCSS.module.css'
 
 const word:string = "testing";
-const guessedLetters:string[]= [];
+const guessedLetters:string[]= ["t", "o", "e"];
 function GuessingWord () {
 
     return  (
             <div className={GuessingWordCSS.wordContainer}> 
                 {word.split('').map((letter, index) => (
-                    <span className={GuessingWordCSS.letter}>
-                        <span className={guessedLetters ? GuessingWordCSS.hidden : ''}>
+                    <span className={GuessingWordCSS.letter} key={index}>
+                        <span className={guessedLetters.includes(letter) ? '' : GuessingWordCSS.hidden} >
                         {letter}
                         </span>
                     </span>
